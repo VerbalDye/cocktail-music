894eca34aad6507c145f334a4ad1b5a84d775eb5
VerbalDye <dhunts258@gmail.com>
2022-03-28 20:58:07 -0400

    added broken post command

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,41 @@ var clientSecret = "d53376ed600341df98c7c3b0c037dc9d";
 var clientKey = "";
 
 var spotifyAccountURL = "https://accounts.spotify.com/authorize?";
+var spotifyTokenURL = "https://accounts.spotify.com/api/token"
 var redirectURL = "http://127.0.0.1:5500/index.html";
 
 
-var redirectToSpotify = function() {
+var redirectToSpotify = function () {
     window.location.href = spotifyAccountURL + "client_id=" + clientID + "&response_type=code&" + "redirect_uri=" + redirectURL;
 }
 
-var getClientKey = function(){
+var getClientKey = function () {
     if (window.location.search.length > 0) {
         clientKey = window.location.search.split("=")[1];
     }
 }
 
-var getPostData = function() {
+var getPostData = function () {
 
+    var postBody = "grant_type=authorization_code";
+    postBody += "&code=" + clientKey;
+    postBody += "&redirect_id=" + redirectURL;
+    postBody += "&client_id" + clientID;
+    postBody += "&client_secret" + clientSecret;
+
+    fetch(spotifyTokenURL, {
+        headers: {
+            'Accept': 'application/json',
+            'Authentication': 'Basic ' + btoa(clientID + ":" +clientSecret),
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+        },
+        method: "POST",
+        body: postBody
+    })
 }
 
 authBtn.addEventListener("click", redirectToSpotify);
